1d3264bda9dbe784f7d94b9988fd07559deb5bd5
Jacob Patel <jseanpatel@gmail.com>
2025-05-15 10:17:30 -0700

    adjust styles

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -104,7 +104,7 @@ export default function About() {
                   />
 
                   <div className="grid grid-cols-1 mt-8 gap-x-8 gap-y-6 sm:grid-cols-2">
-                    <p class="hidden">
+                    <p className="hidden">
                       <label>
                         Don&apos;t fill this out if you&apos;re human:
                         <input name="bot-field" />
